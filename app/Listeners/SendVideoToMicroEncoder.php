2cddfd97db15d4be92c55e0196ca2f3f9aa4d423
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:22:35 -0300

    Implementação SendVideoToMicroEncoder

---
+++ app/Listeners/SendVideoToMicroEncoder.php
@@ -0,0 +1,30 @@
+<?php namespace App\Listeners;
+
+use App\Services\AMQP\AMQPInterface;
+
+class SendVideoToMicroEncoder
+{
+/**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(
+        private AMQPInterface $amqp
+    ) {
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $this->amqp->producerFanout(
+            payload: $event->getPayload(),
+            exchange: config('microservices.micro_encoder_go.exchange')
+        );
+    }
+}
