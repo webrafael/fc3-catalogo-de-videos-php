39e8ca7f30fd3c12750b82b7c64331bb68344aba
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:21:01 -0300

    Melhorias exceptions Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\Response;
+use CatalogVideo\Domain\Exceptions\NotFoundException;
+use CatalogVideo\Domain\Notification\NotificationException;
+use CatalogVideo\Domain\Exceptions\EntityValidationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +30,29 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundException) {
+            return $this->showError($exception->getMessage(), Response::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof EntityValidationException) {
+            return $this->showError($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        if ($exception instanceof NotificationException) {
+            return $this->showError($exception->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        } // ok => HTTP_INTERNAL_SERVER_ERROR
+
+        return parent::render($request, $exception);
+    }
+
+    private function showError(string $message, int $statusCode)
+    {
+        return response()->json([
+            'message' => $message,
+        ], $statusCode);
     }
 }
