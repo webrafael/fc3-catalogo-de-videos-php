d724a126097256365d11322d60d743f87c81dd5f
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:26:01 -0300

    Implementação rotas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\VideoController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CastMemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:api', 'can:admin-catalog'])->group(function () {
+    Route::apiResource('/videos', VideoController::class);
+    Route::apiResource('/categories', CategoryController::class);
+    Route::apiResource('/genres', GenreController::class);
+    Route::apiResource('/cast_members', CastMemberController::class);
+// });
+
+
+Route::get('/', function() {
+    return response()->json(['message' => 'success']);
 });
