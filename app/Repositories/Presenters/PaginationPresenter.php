5bebb344a94736e13c3558ddb9aa8c56b1d3e099
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:25:12 -0300

    Implementação PaginationPresenter

---
+++ app/Repositories/Presenters/PaginationPresenter.php
@@ -0,0 +1,78 @@
+<?php namespace App\Repositories\Presenters;
+
+use CatalogVideo\Domain\Repository\PaginationInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use stdClass;
+
+class PaginationPresenter implements PaginationInterface
+{
+    /** @return stdClass[] */
+    protected array $items = [];
+
+    public function __construct(
+        protected LengthAwarePaginator $paginator
+    ) {
+        $this->items = $this->resolveItems(
+            items: $this->paginator->items()
+        );
+    }
+
+    /**
+     * @return stdClass[]
+     */
+    public function items(): array
+    {
+        return $this->items;
+    }
+
+    public function total(): int
+    {
+        return $this->paginator->total() ?? 0;
+    }
+
+    public function lastPage(): int
+    {
+        return $this->paginator->lastPage() ?? 0;
+    }
+
+    public function firstPage(): int
+    {
+        return $this->paginator->firstItem() ?? 0;
+    }
+
+    public function currentPage(): int
+    {
+        return $this->paginator->currentPage() ?? 0;
+    }
+
+    public function perPage(): int
+    {
+        return $this->paginator->perPage() ?? 0;
+    }
+
+    public function to(): int
+    {
+        return $this->paginator->firstItem() ?? 0;
+    }
+
+    public function from(): int
+    {
+        return $this->paginator->lastItem() ?? 0;
+    }
+
+    private function resolveItems(array $items): array
+    {
+        $response = [];
+
+        foreach ($items as $item) {
+            $stdClass = new stdClass;
+            foreach ($item->toArray() as $key => $value) {
+                $stdClass->{$key} = $value;
+            }
+
+            array_push($response, $stdClass);
+        }
+
+        return $response;
+    }
+}
