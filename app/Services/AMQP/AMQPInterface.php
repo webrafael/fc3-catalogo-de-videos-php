75798fa1ac8c3d20ab2af2fa0fab64b1c5dcd233
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:23:53 -0300

    Implementação AMQPInterface

---
+++ app/Services/AMQP/AMQPInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\AMQP;
+
+use Closure;
+
+interface AMQPInterface
+{
+    public function producer(string $queue, array $payload, string $exchange): void;
+
+    public function producerFanout(array $payload, string $exchange): void;
+
+    public function consumer(string $queue, string $exchange, Closure $callback): void;
+}
