c991a64e5e4ca61da475f869f85c6d835e1d5d61
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:21:46 -0300

    Implementação CategoryResource

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_active' => $this->is_active,
+            'created_at' => Carbon::make($this->created_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
