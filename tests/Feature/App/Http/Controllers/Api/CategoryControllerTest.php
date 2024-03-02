325c7c5082c5c4863c0d4229b0ff6a5281ddcb13
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 07:08:14 -0300

    Implementação testes de integração controllers api

---
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use CatalogVideo\UseCase\Category\CreateCategoryUseCase;
+use CatalogVideo\UseCase\Category\DeleteCategoryUseCase;
+use CatalogVideo\UseCase\Category\ListCategoriesUseCase;
+use CatalogVideo\UseCase\Category\ListCategoryUseCase;
+use CatalogVideo\UseCase\Category\UpdateCategoryUseCase;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+    protected $repository;
+
+    protected $controller;
+
+    protected function setUp(): void
+    {
+        $this->repository = new CategoryEloquentRepository(
+            new Category()
+        );
+        $this->controller = new CategoryController();
+
+        parent::setUp();
+    }
+
+    public function test_index()
+    {
+        $useCase = new ListCategoriesUseCase($this->repository);
+
+        $response = $this->controller->index(new Request(), $useCase);
+
+        $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
+        $this->assertArrayHasKey('meta', $response->additional);
+    }
+
+    public function test_store()
+    {
+        $useCase = new CreateCategoryUseCase($this->repository);
+
+        $request = new StoreCategoryRequest();
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Teste',
+        ]));
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }
+
+    public function test_show()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->controller->show(
+            useCase: new ListCategoryUseCase($this->repository),
+            id: $category->id,
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+    }
+
+    public function test_update()
+    {
+        $category = Category::factory()->create();
+
+        $request = new UpdateCategoryRequest();
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Updated',
+        ]));
+
+        $response = $this->controller->update(
+            request: $request,
+            useCase: new UpdateCategoryUseCase($this->repository),
+            id: $category->id
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+        $this->assertDatabaseHas('categories', [
+            'name' => 'Updated',
+        ]);
+    }
+
+    public function test_delete()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->controller->destroy(
+            useCase: new DeleteCategoryUseCase($this->repository),
+            id: $category->id
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->status());
+    }
+}
