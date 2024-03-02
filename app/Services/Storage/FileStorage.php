6bd294238c0b640143d4e0f2d3ca9f5095566eef
Rafael Andrade <rafael.io@outlook.com>
2024-03-02 06:24:29 -0300

    Implementação FileStorage

---
+++ app/Services/Storage/FileStorage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Storage;
+
+use CatalogVideo\UseCase\Interfaces\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class FileStorage implements FileStorageInterface
+{
+    /**
+     * @param  string  $path
+     * @param  array  $_FILES[file]
+     */
+    public function store(string $path, array $file): string
+    {
+        $contents = $this->convertoFileToLaravelFile($file);
+
+        // return $fileLaravel->store($path);
+        return Storage::put($path, $contents);
+    }
+
+    public function delete(string $path)
+    {
+        Storage::delete($path);
+    }
+
+    protected function convertoFileToLaravelFile(array $file): UploadedFile
+    {
+        return new UploadedFile(
+            path: $file['tmp_name'],
+            originalName: $file['name'],
+            mimeType: $file['type'],
+            error: $file['error'],
+        );
+    }
+}
